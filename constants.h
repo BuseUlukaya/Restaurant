ee90a060852f30b890f0f79fd42741a5372b6ae1
BuseUlukaya <buseulukaya72@gmail.com>
2026-09-18 23:36:08 +0300

    Delete constants.h

--- constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef SENECA_CONSTANTS_H
-#define SENECA_CONSTANTS_H
-#include <iostream>
-namespace gifted {
-   const std::size_t MaximumNumberOfMenuItems = 20u;
-}
-
-#endif // !SENECA_CONSTANTS_H
